c0bffc8572c8649a154c3df336b961562072fee4
Mehrnoosh <me88.sharifi@gmail.com>
2023-01-14 16:02:52 +0330

    Mobile Navigation Modified

--- src/components/layout/MobileHeader/MobileNavigation.js
+++ src/components/layout/MobileHeader/MobileNavigation.js
@@ -10,6 +10,9 @@ import Support from "../NavigationItem/Support";
 import Salesforce from "../NavigationItem/Salesforce";
 import { AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
+import { TbWorld } from "react-icons/tb";
+import { FaUserAlt } from "react-icons/fa";
+import { BsFillTelephoneFill } from "react-icons/bs";
 const items = [
   { id: 1, name: "Product", component: <Product /> },
   { id: 2, name: "Industries", component: <Industries /> },
@@ -32,33 +35,54 @@ const MobileNavigation = () => {
         <FaBars className={`w-5 h-5 ${!isShow ? "block" : "hidden"}`} />
         <AiOutlineClose className={` w-5 h-5 ${isShow ? "block" : "hidden"}`} />
       </div>
-      <div className=" flex flex-col absolute mt-[392px] bg-white border-t ove w-28">
-        {isShow
-          ? items.map((item) => {
-              return (
-                <Popup
-                  trigger={
-                    <button className="cursor-pointer hover:text-blue-600 py-4">
-                      {item.name}
-                    </button>
-                  }
-                  position="right center"
-                  on="click"
-                  closeOnDocumentClick
-                  mouseLeaveDelay={100}
-                  mouseEnterDelay={0}
-                  contentStyle={{ padding: "0px", border: "none" }}
-                  arrow={false}
-                  key={item.id}
-                >
-                  <div className="w-auto bg-white h-28 mt-14 p-4 overflow-auto ">
-                    {item.component}
-                  </div>
-                </Popup>
-              );
-            })
-          : ""}
-      </div>
+      {isShow ? (
+        <div className=" flex flex-col absolute mt-[264px] bg-white overflow-y-auto h-52">
+          {items.map((item) => {
+            return (
+              <Popup
+                trigger={
+                  <button className="cursor-pointer text-lg font-bold hover:text-blue-600 py-2">
+                    {item.name}
+                  </button>
+                }
+                position="right center"
+                on="click"
+                closeOnDocumentClick
+                mouseLeaveDelay={100}
+                mouseEnterDelay={0}
+                contentStyle={{ padding: "0px", border: "none" }}
+                arrow={false}
+                key={item.id}
+              >
+                <div className="w-auto bg-white h-28 mt-14 p-4 overflow-auto ">
+                  {item.component}
+                </div>
+              </Popup>
+            );
+          })}
+          <div className="mt-6">
+            <div className="cursor-pointer py-2 px-3 flex items-center hover:bg-blue-100 hover:rounded-3xl hover:text-blue-600">
+              <BsFillTelephoneFill />
+              <NavLink
+                to="/"
+                className=" ml-1 font-medium cursor-pointer hover:text-blue-600"
+              >
+                Contact
+              </NavLink>
+            </div>
+            <div className="cursor-pointer py-2 px-3 flex items-center hover:bg-blue-100 hover:rounded-3xl hover:text-blue-600">
+              <FaUserAlt />
+              <NavLink className="ml-1 font-medium">Login</NavLink>
+            </div>
+            <div className="cursor-pointer py-2 px-3 flex items-center hover:bg-blue-100 hover:rounded-3xl hover:text-blue-600">
+              <TbWorld className="w-6 h-6  hover:text-blue-600" />
+              <NavLink className="ml-1 font-medium">Region</NavLink>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
       <ul className="flex   text-lg font-medium text-slate-800 justify-between items-center">
         <NavLink to="/" className="cursor-pointer shrink-0">
           <img src={image} alt="logo" className="w-11 h-15 " />
