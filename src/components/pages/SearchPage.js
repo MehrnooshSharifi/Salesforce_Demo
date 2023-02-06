88c084ea53af9639008acc2892db9f6911f7efe7
Mehrnoosh <me88.sharifi@gmail.com>
2023-02-06 22:47:50 +0330

    search Page Modified

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -3,16 +3,20 @@ import { FaSearch } from "react-icons/fa";
 import { useEffect, useRef } from "react";
 
 const SearchPage = () => {
-    const inputRef=useRef();
-    useEffect(()=>{
-        inputRef.current.focus();
-    } , [])
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <Layout>
-      <div className=" bg-gray-100 w-full h-screen">
-        <div className="w-full flex items-center justify-center mt-8 p-8 border-b border-b-blue-900 container mx-auto" >
-          <FaSearch className="w-8 h-8 mr-4 text-blue-800" />
-          <input ref={inputRef} type="text" className="w-full p-3 outline-none bg-gray-100  text-5xl" />
+      <div className=" bg-gray-100 w-full h-screen flex flex-col">
+        <div className=" border-2 border-blue-900 rounded-md lg:rounded-none p-1 container mx-auto lg:w-full mt-9  flex items-center justify-center lg:mt-8 lg:p-8 lg:border-0 lg:border-b lg:border-b-blue-900 ">
+          <FaSearch className="lg:w-8 lg:h-8 mr-4 text-blue-800" />
+          <input
+            ref={inputRef}
+            type="text"
+            className="w-full bg-gray-100 outline-none lg:p-3 lg:outline-none lg:bg-gray-100  lg:text-5xl"
+          />
         </div>
       </div>
     </Layout>
